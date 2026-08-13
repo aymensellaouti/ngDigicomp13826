31146bbd6f3b3e6e308096e649810ca75ae8af17
aymensellaouti <aymen.sellaouti@gmail.com>
2026-08-13 14:13:22 +0100

    start exercice TTC

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,12 +6,13 @@ import { Two } from './components/two/two';
 import { RotatingCard } from "./components/rotating-card/rotating-card";
 import { Counter } from "./signal/counter/counter";
 import { Sum } from "./signal/sum/sum";
+import { TtcComponent } from "./signal/ttc/ttc.component";
 
 @Component({
   // Comment j'appelle mon composant
   selector: 'app-root',
   // je définit ce dont j'ai besoin mes dépendances : composants, directives, pipe
-  imports: [Two, RotatingCard, Counter, Color, Sum],
+  imports: [Two, RotatingCard, Counter, Color, Sum, TtcComponent],
   // Le bout de code html géré par ce composant
   templateUrl: './app.html',
   // Le style css appliqué à ce composant
