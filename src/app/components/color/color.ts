d6b89e4326956b9f9a047296de9269dae593cde0
aymensellaouti <aymen.sellaouti@gmail.com>
2026-08-13 13:15:33 +0100

    color component correction de l'exo

--- src/app/components/color/color.ts
+++ src/app/components/color/color.ts
@@ -6,4 +6,18 @@ import { Component } from '@angular/core';
   templateUrl: './color.html',
   styleUrl: './color.css',
 })
-export class Color {}
+export class Color {
+  // State : attributs dynamique de la vue
+  defaultColor = 'red';
+  /**
+   * @var représente la couleur du background
+   */
+  color = this.defaultColor;
+  changeColor(newColorInput: HTMLInputElement) {
+    this.color = newColorInput.value;
+    newColorInput.value = '';
+  }
+  reset() {
+    this.color = this.defaultColor;
+  }
+}
