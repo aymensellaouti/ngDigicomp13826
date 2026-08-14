da00d7fe5ba5145468f726727b3e9c034f861502
aymensellaouti <aymen.sellaouti@gmail.com>
2026-08-14 14:32:47 +0100

    fin router

--- src/app/cv/details-cv/details-cv.component.ts
+++ src/app/cv/details-cv/details-cv.component.ts
@@ -1,27 +1,41 @@
 import { Component, inject, signal } from "@angular/core";
 import { Cv } from "../model/cv.model";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
+import { CvService } from "../services/cv-service";
+import { DefaultImagePipe } from "../pipes/default-image-pipe";
 
 @Component({
   selector: 'app-details-cv',
   templateUrl: './details-cv.component.html',
   styleUrls: ['./details-cv.component.css'],
+  imports: [DefaultImagePipe],
 })
 export class DetailsCvComponent {
   // cv/1 => je dois récupérer le paramètre id
   // 1- Récupérer le service
   acr = inject(ActivatedRoute);
-  cv= signal<Cv | null>(null);
-
+  cv = signal<Cv | null>(null);
+  cvService = inject(CvService);
+  router = inject(Router);
   constructor() {
     console.log(this.acr.snapshot.params);
     // Todos:
     // récupérer le id
     const id = this.acr.snapshot.params['id'];
     // récupérer le cv d'id id
-      // Si je le trouve
-        // je le met dans le signal cv
-      // Sinon
-        // Rediriger vers la liste des cvs (Router qu'il faudra injecter)
+    // Si je le trouve
+    // je le met dans le signal cv
+    this.cv.set(this.cvService.findCvById(id));
+    // Sinon
+    if (!this.cv())
+      // Rediriger vers la liste des cvs (Router qu'il faudra injecter)
+      this.router.navigate(['cv']);
+  }
+  delete() {
+    const cv = this.cv();
+    if (cv) {
+      this.cvService.deleteCv(cv);
+      this.router.navigate(['cv']);
+    }
   }
 }
