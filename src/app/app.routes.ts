bbd606f66095a88b1dd2c7d77cd8ae5eb24ca424
aymensellaouti <aymen.sellaouti@gmail.com>
2026-08-14 14:51:14 +0100

    exemple form login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { TodoComponent } from './todo/todo/todo.component';
 import { MiniWordComponent } from './directives/mini-word/mini-word.component';
 import { DetailsCvComponent } from './cv/details-cv/details-cv.component';
 import { NF404 } from './components/nf404/nf404';
+import { Login } from './auth/login/login';
 
 export const routes: Routes = [
   {
@@ -17,6 +18,7 @@ export const routes: Routes = [
   // avec : devant le id on indique que id est variable
   { path: 'cv/:id', component: DetailsCvComponent},
   { path: 'todo', component: TodoComponent},
+  { path: 'login', component: Login},
   { path: 'word', component: MiniWordComponent},
   { path: '**', component: NF404}
 ];
--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+login(form: NgForm) {
+  console.log(form);
+}
+}
